83b70d0736945ae0217b178678b5fc8e0a351a2c
Ethan S. Chan <eschan@andrew.cmu,edu>
2013-02-17 23:30:13 -0500

    First Commit

---
+++ Parse.js
@@ -0,0 +1,32 @@
+Components.utils.import("resource://gre/modules/jsdebugger.jsm");
+var myDebugger = new Debugger(lines);
+
+var code = ...;
+var lines = code.split("\n");
+var numberOfLines = lines.length;
+
+
+function isComment(line) {
+
+};
+
+function isFunction(line) {
+
+};
+
+function isLoop(line) { 
+
+};
+
+
+
+for (i = 0; i < numberOfLines; i++) {
+	var line = lines[i]
+	//Check if Comment?
+
+	//Check if Function
+
+	//Check if Loop
+
+
+};
