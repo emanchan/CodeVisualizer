4ff7a3ce315bf04078bb8042270d0d201ce43a21
Jonathan Leung <jonlhk@gmail.com>
2013-02-24 18:16:09 -0500

    Moved clousure stuff into compileCode.js

---
+++ static/compileCode.js
@@ -0,0 +1,83 @@
+//This file interactes with google closure compler and update DOM
+
+function sendCode () {
+  console.log("Sending!");
+  var js_code = $("#js_code").val();
+  var compilation_level = $("#compilation_level").val();
+  var output_format = $("#output_format").val();
+  compileCode(js_code, compilation_level);
+}
+
+function compileCode(js_code, compilation_level) {
+  getCompiledCode(js_code, compilation_level);
+  getErrors(js_code, compilation_level);
+  getWarnings(js_code, compilation_level);
+  getStatistics(js_code, compilation_level);
+  updateCompiledCode();
+}
+
+//Retrives compiled code and checks for server errors
+function getCompiledCode(js_code, compilation_level) {
+  $.ajax({
+    type: "post",
+    dataType: "json",
+    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"compiled_code", 'formatting':"pretty_print"},
+    url: "http://closure-compiler.appspot.com/compile",
+    success: function (data) {
+      if(data.compiledCode === undefined){
+        return data.serverErrors[0].error;
+      } else {
+        return data.compiledCode;
+      }
+      console.log("sent!");
+    }
+  });
+}
+
+//Retrives errors if there are any
+function getErrors(js_code, compilation_level) {
+  $.ajax({
+    type: "post",
+    dataType: "json",
+    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"errors"},
+    url: "http://closure-compiler.appspot.com/compile",
+    success: function (data) {
+      if(data.errors !== undefined){
+        var errorMSG = "Compile errors:\n";
+        for(var i = 0; i < data.errors.length; i++) {
+          errorMSG += "Line: " + data.errors[i].lineno + '\n';
+          errorMSG += "  " + data.errors[i].error + '\n';
+        }
+        return errorMSG;
+      }
+    }
+  });
+}
+
+function getWarnings(js_code, compilation_level) {
+  $.ajax({
+    type: "post",
+    dataType: "json",
+    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"warnings"},
+    url: "http://closure-compiler.appspot.com/compile",
+    success: function (data) {
+      //TODO
+    }
+  });
+}
+
+function getStatistics(js_code, compilation_level) {
+  $.ajax({
+    type: "post",
+    dataType: "json",
+    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"statistics"},
+    url: "http://closure-compiler.appspot.com/compile",
+    success: function (data) {
+      //TODO
+    }
+  });
+}
+
+function updateCompiledCode(code) {
+  $("#code_content").html(code);
+}
--- static/main.js
+++ static/main.js
@@ -2,89 +2,6 @@
 var localDatabase = {}; // Holds the information for the user's files
 var currentUser = "testUser";
 
-//Sends code to google closure compiler
-function sendCode () {
-  console.log("Sending!");
-  var js_code = $("#js_code").val();
-  var compilation_level = $("#compilation_level").val();
-  var output_format = $("#output_format").val();
-  compileCode(js_code, compilation_level);
-}
-
-function compileCode(js_code, compilation_level) {
-  getCompiledCode(js_code, compilation_level);
-  getErrors(js_code, compilation_level);
-  getWarnings(js_code, compilation_level);
-  getStatistics(js_code, compilation_level);
-  updateCompiledCode();
-}
-
-//Retrives compiled code and checks for server errors
-function getCompiledCode(js_code, compilation_level) {
-  $.ajax({
-    type: "post",
-    dataType: "json",
-    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"compiled_code", 'formatting':"pretty_print"},
-    url: "http://closure-compiler.appspot.com/compile",
-    success: function (data) {
-      if(data.compiledCode === undefined){
-        return data.serverErrors[0].error;
-      } else {
-        return data.compiledCode;
-      }
-      console.log("sent!");
-    }
-  });
-}
-
-//Retrives errors if there are any
-function getErrors(js_code, compilation_level) {
-  $.ajax({
-    type: "post",
-    dataType: "json",
-    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"errors"},
-    url: "http://closure-compiler.appspot.com/compile",
-    success: function (data) {
-      if(data.errors !== undefined){
-        var errorMSG = "Compile errors:\n";
-        for(var i = 0; i < data.errors.length; i++) {
-          errorMSG += "Line: " + data.errors[i].lineno + '\n';
-          errorMSG += "  " + data.errors[i].error + '\n';
-        }
-        return errorMSG;
-      }
-    }
-  });
-}
-
-function getWarnings(js_code, compilation_level) {
-  $.ajax({
-    type: "post",
-    dataType: "json",
-    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"warnings"},
-    url: "http://closure-compiler.appspot.com/compile",
-    success: function (data) {
-      //TODO
-    }
-  });
-}
-
-function getStatistics(js_code, compilation_level) {
-  $.ajax({
-    type: "post",
-    dataType: "json",
-    data: {'js_code' : js_code, 'compilation_level':compilation_level, 'output_format':"json", 'output_info':"statistics"},
-    url: "http://closure-compiler.appspot.com/compile",
-    success: function (data) {
-      //TODO
-    }
-  });
-}
-
-function updateCompiledCode(code) {
-  $("#code_content").html(code);
-}
-
 function getFiles(username) {
   $.ajax({
     type: "get",
@@ -137,16 +54,8 @@ $(document).ready(function () {
       minWidth: 200,
       resize: function(event, ui){
           var currentWidth = ui.size.width;
-          
-          // this accounts for padding in the panels + 
-          // borders, you could calculate this using jQuery
           var padding = 12; 
-          
-          // this accounts for some lag in the ui.size value, if you take this away 
-          // you'll get some instable behaviour
           $(this).width(currentWidth);
-          
-          // set the content panel width
           $("#rpanel").width(contentWidth - currentWidth);            
       }
 	});
@@ -158,6 +67,7 @@ $(document).ready(function () {
 		$("#rpanel").width(contentWidth - $("#lpanel").width());
 	});
 
+  //Handles switching between code info and visualizer in right panel
   $("#visualizer").click(function () {
     if($("#code_content" === undefined)){
       var info = $("#info_area").empty();
